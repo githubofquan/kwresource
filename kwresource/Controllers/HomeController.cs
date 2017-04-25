e0b1ca9997f29a98581348c490f9ba70974129ab
Quan <mailofquan@gmail.com>
2017-04-25 14:30:15 +0700

    first commit

--- kwresource/Controllers/HomeController.cs
+++ kwresource/Controllers/HomeController.cs
@@ -3,16 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
+using kwresource.Models;
+using System.ComponentModel;
+using System.Threading.Tasks;
 
 namespace kwresource.Controllers
 {
     public class HomeController : Controller
     {
+        private kwresourceEntities db = new kwresourceEntities();
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
 
             return View();
+        }
+
+        public async Task<string> GetForm(FormData formData)
+        {          
+            var result = formData.field1 + "_" + formData.field2;
+            var kw = new keyword()
+            {
+                Id = Guid.NewGuid().ToString(),
+                kw = result
+            };
+            db.keywords.Add(kw);
+            db.SaveChanges();
+            return result;
+        }
+
+        public class FormData
+        {
+            public string field1 { get; set; }
+            public string field2 { get; set; }
         }
     }
 }
